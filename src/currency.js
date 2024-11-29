b77b0c3ddd741b353929f60a85aeff5f2a116a19
Anastasia <127306365+nnnnastia@users.noreply.github.com>
2024-11-30 01:09:23 +0200

    Update currency.js

--- src/currency.js
+++ src/currency.js
@@ -1,31 +1,13 @@
 // src/currency.js
 
-/**
- * Перетворює рядок з валютою на число.
- * @param {string} value - Наприклад, "$50".
- * @returns {number} - Наприклад, 50.
- */
 function parseCurrency(value) {
     return parseFloat(value.replace('$', ''));
 }
 
-/**
- * Форматує число у формат валюти.
- * @param {number} value - Наприклад, 100.
- * @returns {string} - Наприклад, "$100".
- */
 function formatCurrency(value) {
     return `$${value.toFixed(2)}`;
 }
 
-/**
- * Додає два значення валюти.
- * інтегрує дві попередні функції 
- * для додавання форматованих значень.
- * @param {string} value1 - Наприклад, "$50".
- * @param {string} value2 - Наприклад, "$30".
- * @returns {string} - Наприклад, "$80".
- */
 function addCurrencies(value1, value2) {
     const num1 = parseCurrency(value1);
     const num2 = parseCurrency(value2);
