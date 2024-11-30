d6b7073bc6022b972bfc5cc9143d08f6ece09044
Anastasia <127306365+nnnnastia@users.noreply.github.com>
2024-11-30 20:11:45 +0200

    Delete src/tests directory

--- src/tests/auth.test.js
+++
@@ -1,7 +0,0 @@
-// tests/auth.test.js
-const isPasswordValid = require('../src/auth');
-
-test('Перевірка валідності пароля', () => {
-    expect(isPasswordValid('password123')).toBe(true); // Валідний пароль
-    expect(isPasswordValid('short')).toBe(false); // Занадто короткий пароль
-});
